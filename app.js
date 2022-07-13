ef4c3eaab59803c680f16c47ff7857312a62ea26
unknown <daveasmah@yahoo.com>
2022-07-12 21:59:52 -0400

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 // Define a function that takes in an array of elements and joins
-// them together into a string separated by a comma with a space 
-//after the comma.
+// them together into a string separated by a comma with a space after the comma.
+
+// Declaring an array of cars
+let arrayOfCars = ["BMW", "Mercedes-Benz", "Jaguar", "Maserati", "Audi"];
+
+//Writing a function with an array as a parameter
+function changeToString(arr){
+
+ //using the .join() method with a comma and space as a separator  
+    return arr.join(", ");
+}
+
+console.log(changeToString(arrayOfCars));
